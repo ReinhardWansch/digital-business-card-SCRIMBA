4f6fae2e8670cd3329482580a844329589c80f86
Reinhard Wansch <reinhard@wansch.at>
2026-02-05 10:52:05 +0100

    add font via npm

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Footer from "./components/Footer"
 function App() {
 
   return (
-    <div id="pageCtn" className="bg-bg-page min-h-screen flex justify-center items-center text-[0.64rem] font-sans">
+    <div id="pageCtn" className="bg-bg-page min-h-screen flex justify-center items-center text-[0.64rem] font-card">
       <div id="card" className="bg-bg-card w-80 rounded-lg overflow-hidden">
         <Info />
         <About />
